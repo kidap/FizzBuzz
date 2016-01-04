d02435c400c2371249e966d6e207f6ec93b72f34
Karlo Pagtakhan <kidap@yahoo.com>
2016-01-04 15:47:17 -0500

    Working code

--- FizzBizz/main.c
+++ FizzBizz/main.c
@@ -9,7 +9,19 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
+    //Loop
+    for (int x=1; x<=100; x++) {
+        if( x % 3 == 0 && x % 5 == 0 ){
+            printf("FizzBizz \n");
+        } else if( x % 3 == 0 ){
+            printf("Fizz \n");
+        } else if (x % 5 == 0) {
+            printf("Bizz \n");
+        }
+        else {
+            printf("%d \n",x);
+        }
+    }
     return 0;
 }
